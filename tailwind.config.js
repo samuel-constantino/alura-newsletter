8b5214b91e530fe7bdfbaa792d14f0307fce9113
Samuel Constantino <samuelicapuidados@gmail.com>
2022-05-06 15:00:11 -0300

    feat: add personal animate

--- tailwind.config.js
+++ tailwind.config.js
@@ -109,12 +109,23 @@ module.exports = {
         80: '20rem',
         96: '24rem',
       },
+      keyframes: {
+        sino_kf: {
+          '0%, 100%': {
+            transform: 'rotate(-10deg)'
+          },
+          '50%': {
+            transform: 'rotate(10deg)'
+          }
+        }
+      },
       animation: {
         none: 'none',
         spin: 'spin 1s linear infinite',
         ping: 'ping 1s cubic-bezier(0, 0, 0.2, 1) infinite',
         pulse: 'pulse 2s cubic-bezier(0.4, 0, 0.6, 1) infinite',
         bounce: 'bounce 1s infinite',
+        sino: 'sino_kf 0.31s ease-in-out infinite'
       },
       aspectRatio: {
         auto: 'auto',
