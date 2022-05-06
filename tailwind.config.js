1cbec7cf7fdf28451a62249b217cbc500aa77e8e
Samuel Constantino <samuelicapuidados@gmail.com>
2022-05-06 10:34:50 -0300

    feat: install and configure tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  content: ["./*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        azul: {
+            claro: '#C5DFFF',
+            escuro: '#061E3C',
+            hover: '#1057B0'
+        },
+      },
+      fontFamily: {
+          inter: ['Inter', 'sans-serif']    
+      }
+    },
+  },
+  plugins: [],
+}
